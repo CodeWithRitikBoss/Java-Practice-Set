598debebc7f05c660fc6de0fd79577e17469a429
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2025-05-01 23:18:24 +0530

    Code commited.

---
+++ PracticeSet5.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class PracticeSet5 {
+    public static void main(String[] args) {
+        System.out.println("Practice set -5.");
+
+        // Chapter 5.
+
+        // // Question 1.
+        // // Write a program to print the following pattern:
+        //     // * * * * *
+        //     // * * * *
+        //     // * * *
+        //     // * *
+        //     // *
+        
+        // for(int i= 0; i<5; i++){
+        //     for(int j= 5; j>i; j--){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // // Question 2.
+        // // Write a program to sum first n even numbers using a while loop.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // int n = sc.nextInt();
+        // int i = 1;
+        // int sum = 0;
+        // while (i <= n) {
+        //     sum += 2 * i; // Add the even number to the sum
+        //     i++; // Increment i to get the next even number
+        // }
+        // System.out.println("The sum of first " + n + " even numbers is: " + sum);
+
+        // // Question 3.
+        // // Write a program to print multiplication table of a given number n.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // int n = sc.nextInt();
+        // System.out.println("Multiplication table of " + n + ": ");
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println(n + " x " + i + " = " + (n * i));
+        // }
+
+        // // Question 4.
+        // // Write a program to print multiplication table of 10 in reverse order.
+        // for (int i = 10; i >= 1; i--) {
+        //     System.out.println("10 x " + i + " = " + (10 * i));
+        // }
+
+        // // Question 5.
+        // // Write a program to find factorial of a number using a for loop.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // int n = sc.nextInt();
+        // int factorial = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     factorial *= i; // Multiply the current value of factorial by i
+        // }
+        // System.out.println("The factorial of " + n + " is: " + factorial);
+
+        // // Question 6.
+        // // Repeat 5 using for loop.
+        // for (int j = 1; j <= 10; j++) {
+        //     System.out.println("5");
+        // }
+
+        // // Question 7.
+        // // Repeat 1 using for/while loop.
+        // for(int i= 0; i<5; i++){
+        //     System.out.println(1);
+        // }
+        // int i= 0;
+        // while(i<5){
+        //     System.out.println(1);
+        //     i++;
+        // }
+
+        // // Question 8.
+        // // What can be done using one type of loop can also be done using the other two types of loops. (Ture or False).
+        // System.out.println("True. What can be done using one type of loop can also be done using the other two types of loops.");
+        // // This is because all three types of loops (for, while, and do-while) can be used to achieve the same functionality, but they may differ in syntax and structure. The choice of loop often depends on the specific use case and personal preference.
+
+        // // Question 9.
+        // // Write a program to calculate the sum of the numbers occuring in the multiplication table of 8.
+        // int sum = 0;
+        // for (int i = 1; i <= 10; i++) {
+        //     sum+= 8 * i; // Add the current multiplication result to the sum
+        // }
+        // System.out.println("The sum of the numbers occurring in the multiplication table of 8 is: " + sum);
+
+        // // Question 10.
+        //     // A do while loop is excuted.
+        //     // 1-> At least once.
+        //     // 2-> At least twice.
+        //     // 3-> At least thrice.
+        // System.out.println("Option 1-> At least once.");
+
+        // // Question 11.
+        // // Repeat 2 using for loop.
+        // for(int i= 0; i<5; i++){
+        //     System.out.println(2);
+        // }
+        
+    }
+}
