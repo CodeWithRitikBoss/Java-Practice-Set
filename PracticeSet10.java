f07ea95b65b1197ac8b61c27a6fe2425bed2c7ea
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2025-05-02 23:28:12 +0530

    Add inheritance example with Base, Derived, and FurtherDerived classes to PracticeSet10.java

--- PracticeSet10.java
+++ PracticeSet10.java
@@ -115,37 +115,57 @@
 //     }
 // }
 
-// Class for question 4.
-class Rectangle {
-    private double length; // Property to store the length
-    private double width; // Property to store the width
+// // Class for question 4.
+// class Rectangle {
+//     private double length; // Property to store the length
+//     private double width; // Property to store the width
 
-    // Constructor to initialize the rectangle with length and width
-    public Rectangle(double length, double width) {
-        this.length = length; // Set the length property
-        this.width = width; // Set the width property
-    }
+//     // Constructor to initialize the rectangle with length and width
+//     public Rectangle(double length, double width) {
+//         this.length = length; // Set the length property
+//         this.width = width; // Set the width property
+//     }
 
-    // Method to get the length of the rectangle
-    public double getLength() {
-        return length; // Return the length property
-    }
+//     // Method to get the length of the rectangle
+//     public double getLength() {
+//         return length; // Return the length property
+//     }
 
-    // Method to get the width of the rectangle
-    public double getWidth() {
-        return width; // Return the width property
-    }
+//     // Method to get the width of the rectangle
+//     public double getWidth() {
+//         return width; // Return the width property
+//     }
 
-    // Method to calculate the area of the rectangle
-    public double calculateArea() {
-        return length * width; // Area = length * width
-    }
+//     // Method to calculate the area of the rectangle
+//     public double calculateArea() {
+//         return length * width; // Area = length * width
+//     }
 
-    // Method to calculate the volume of the rectangle (treated as a cube)
-    public double calculateVolume() {
-        return length * width * length; // Volume = length * width * height (height is treated as length for a cube)
-    }
-}
+//     // Method to calculate the volume of the rectangle (treated as a cube)
+//     public double calculateVolume() {
+//         return length * width * length; // Volume = length * width * height (height is treated as length for a cube)
+//     }
+// }
+
+// // Class for question 5.
+// class Base {
+//     // Base class constructor
+//     public Base() {
+//         System.out.println("Base class constructor called.");
+//     }
+// }
+// class Derived extends Base {
+//     // Derived class constructor
+//     public Derived() {
+//         System.out.println("Derived class constructor called.");
+//     }
+// }
+// class FurtherDerived extends Derived {
+//     // FurtherDerived class constructor
+//     public FurtherDerived() {
+//         System.out.println("FurtherDerived class constructor called.");
+//     }
+// }
 
 
 
@@ -188,7 +208,18 @@ public class PracticeSet10 {
         // System.out.println("Rectangle area: " + rectangle.calculateArea()); // Calculate the area of the rectangle
         // System.out.println("Rectangle volume: " + rectangle.calculateVolume()); // Calculate the volume of the rectangle
 
-
+        // // Question 5.
+        // // What is the order of constructor execution for the following inheritance hierarchy?
+        // // class Base {}
+        // // class Derived extends Base {}
+        // // class FurtherDerived extends Derived {}
+        // // Answer: The order of constructor execution is as follows:
+        // // 1. Base constructor is executed first.
+        // // 2. Derived constructor is executed next.
+        // // 3. FurtherDerived constructor is executed last.
+        // // This is because in Java, the constructor of the superclass is always called before the constructor of the subclass.
+        // FurtherDerived obj = new FurtherDerived(); // Create an object of the FurtherDerived class
+        // // This will trigger the constructor execution in the order: Base -> Derived -> FurtherDerived
 
     }
 }
