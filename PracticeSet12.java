3643b266f2b004d979f9dd09fafc8b44582c50d2
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2025-05-03 00:31:52 +0530

    Add PracticeSet12.java with a main method and initial structure

---
+++ PracticeSet12.java
@@ -0,0 +1,10 @@
+public class PracticeSet12 {
+    public static void main(String[] args) {
+        System.out.println("Practice Set - 12");
+
+        // Chapter 12.
+
+        // Question 1.
+        // 
+    }
+}
